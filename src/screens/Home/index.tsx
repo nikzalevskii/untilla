540b37579e24daedab2cc8ca766c936268dea302
Nikita Zalevskii <nikita.zalevsky@amigochat.io>
2026-02-21 18:20:37 +0300

    useTranslation in HomeScreen

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,13 +1,15 @@
 import { Text, View } from 'react-native'
 import { useStyles } from './styles'
+import { useTranslation } from 'react-i18next'
 
 export const HomeScreen = () => {
   const styles = useStyles()
+  const { t } = useTranslation()
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Home</Text>
-      <Text style={styles.subtitle}>Your countdowns will appear here</Text>
+      <Text style={styles.title}>{t('home.title')}</Text>
+      <Text style={styles.subtitle}>{t('home.subtitle')}</Text>
     </View>
   )
 }
