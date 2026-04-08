1810a920971ac4ca9dfa153ded28486321fefb5a
Nikita Zalevskii <nzalevskii@gmail.com>
2026-04-08 13:48:50 +0300

    corrected DEFAULT_THEME

--- src/constants/themes.ts
+++ src/constants/themes.ts
@@ -4,4 +4,4 @@ import { countdownThemes } from '@/types/countdown'
 // Order = display order in ThemePicker
 export const CARD_THEMES = countdownThemes
 
-export const DEFAULT_THEME: CountdownTheme = 'violet'
+export const DEFAULT_THEME = 'violet' as const satisfies (typeof countdownThemes)[number]
